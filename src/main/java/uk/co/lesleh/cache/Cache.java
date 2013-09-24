cdd659ccb8f57008f422adeafaf708b08260db67
Leslie Hoare <iam@lesleh.co.uk>
2013-09-24 11:09:31 +0100

    Add Cache interface.

---
+++ src/main/java/uk/co/lesleh/cache/Cache.java
@@ -0,0 +1,14 @@
+package uk.co.lesleh.cache;
+
+public interface Cache<E> {
+
+    E get(String key);
+
+    void set(String key, E element, int size);
+
+    void remove(String key);
+
+    boolean has(String key);
+
+    void flush();
+}
