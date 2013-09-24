a96055c688ba7b880801848becb951048b7f4a93
Leslie Hoare <iam@lesleh.co.uk>
2013-09-24 18:01:20 +0100

    Use value length to calculate the element size instead.

--- src/test/java/uk/co/lesleh/cache/LruMemoryCacheTests.java
+++ src/test/java/uk/co/lesleh/cache/LruMemoryCacheTests.java
@@ -6,9 +6,7 @@ import org.junit.*;
 
 public class LruMemoryCacheTests {
 
-    private final int MAX_SIZE = 250;
-
-    private final int ELEMENT_SIZE = 100;
+    private final int MAX_SIZE = 25;
 
     Cache<String> cache;
 
@@ -17,23 +15,23 @@ public class LruMemoryCacheTests {
         cache = new LruMemoryCache<String>(MAX_SIZE) {
             @Override
             public int calculateSize(String element) {
-                return ELEMENT_SIZE;
+                return element.length();
             }
         };
     }
 
     @Test
     public void testSet() {
-        cache.set("a", "aaa");
+        cache.set("a", "a");
         assertTrue("Cache should contain 'a'.", cache.containsKey("a"));
-        assertEquals(100, cache.size());
     }
 
     @Test
     public void testSettingKeyAgain() {
-        cache.set("a", "aaa");
-        cache.set("a", "bbb");
-        assertEquals("bbb", cache.get("a"));
+        cache.set("a", "a");
+        cache.set("a", "bb");
+        assertEquals("bb", cache.get("a"));
+        assertEquals(2, cache.size());
     }
 
     @Test
@@ -45,28 +43,28 @@ public class LruMemoryCacheTests {
 
     @Test
     public void testRemovalWhenFull() {
-        cache.set("a", "aaa");
-        cache.set("b", "bbb");
-        cache.set("c", "ccc");
+        cache.set("a", "1234567890");
+        cache.set("b", "1234567890");
+        cache.set("c", "1234567890");
 
         assertFalse(cache.containsKey("a"));
     }
 
     @Test
     public void testRemovalOfLruWhenFull() {
-        cache.set("a", "aaa");
-        cache.set("b", "bbb");
+        cache.set("a", "1234567890");
+        cache.set("b", "1234567890");
 
         cache.get("a"); // Refresh "a"
 
-        cache.set("c", "ccc");
+        cache.set("c", "1234567890");
         assertFalse(cache.containsKey("b"));
     }
 
     @Test
     public void testClear() {
-        cache.set("a", "aaa");
-        cache.set("b", "bbb");
+        cache.set("a", "a");
+        cache.set("b", "b");
 
         cache.clear();
         assertEquals(0, cache.size());
@@ -76,10 +74,10 @@ public class LruMemoryCacheTests {
 
     @Test
     public void testSize() {
-        cache.set("a", "aaa");
-        assertEquals(ELEMENT_SIZE * 1, cache.size());
-        cache.set("b", "bbb");
-        assertEquals(ELEMENT_SIZE * 2, cache.size());
+        cache.set("a", "a");
+        assertEquals(1, cache.size());
+        cache.set("b", "bb");
+        assertEquals(3, cache.size());
     }
 
     @Test(expected = NullPointerException.class)
@@ -89,7 +87,7 @@ public class LruMemoryCacheTests {
 
     @Test(expected = NullPointerException.class)
     public void testSetNullKey() {
-        cache.set(null, "aaa");
+        cache.set(null, "a");
     }
 
     @Test(expected = NullPointerException.class)
